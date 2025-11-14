f338d88621598fc0cd6887d4c0370b0af5bc7084
Basir Naji <basirnaji007@gmail.com>
2025-11-14 21:41:22 +0430

    remove created

--- Array.java
+++ Array.java
@@ -20,7 +20,7 @@ public class Array {
         // if the array is full , resize it
         if (count == items.length) {
             // create the new array ( size + 1)
-            int[] newItems = new int[count + 1];
+            int[] newItems = new int[count * 2];
             // copy all the existing items
             for (int i = 0; i < count; i++)
                 newItems[i] = items[i];
@@ -28,8 +28,26 @@ public class Array {
             items = newItems;
 
         }
-        // add new item at the end
+        // add new item at the endr
         items[count++] = item;
+    }
+
+    // [10,20,30,40,50]
+    public void removeAt(int index) {
+        // last index
+
+        // validate
+        if (index < 0 || index > items.length - 1)
+            throw new IllegalArgumentException();
+
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+        count--;
+    }
+
+    public void removeAt() {
+        items[count - 1] = 0;
+        count--;
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var numbers = new Array(3);
+        var numbers = new Array(4);
         numbers.insert(10);
         numbers.insert(20);
         numbers.insert(30);
-        numbers.insert(30);
-        numbers.insert(30);
+        numbers.insert(50);
+        numbers.removeAt(5);
+        numbers.removeAt();
         numbers.print();
     }
 }
